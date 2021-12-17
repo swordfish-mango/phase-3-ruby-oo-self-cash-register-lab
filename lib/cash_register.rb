22d4d6eea7496c9ed1ec486b6075c5963ae463f4
Marcus <marcus.shimotsu@gmail.com>
2021-12-17 11:00:38 -0600

    commit tree 64315a90e68c8602bfc3d11c19f8db26240b0e95

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+class CashRegister
+    attr_accessor :total
+    attr_accessor :discount
+    attr_accessor :prev_total
+    attr_accessor :items
+
+    def initialize(employee_discount=0)
+        @total = 0
+        @discount = employee_discount
+        @items = []
+    end
+
+    def add_item(title,price,quantity=1)
+        @prev_total = total
+        cost = price*quantity
+        @total = @total + price*quantity
+        @items = @items.push(title)
+    end
+
+    def apply_discount
+        if discount == 0
+            return "There is no discount to apply."
+        else
+            @total = @total * (100-discount) / 100
+            return "After the discount, the total comes to $#{total}."
+        end
+    end
+
+    def void_last_transaction
+        @total = @prev_total
+    end
+end
+
+
 
